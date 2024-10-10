5cb81ddcf446c13ae35a07c94bbfd87f741407b1
Dikshant Kumar Singh <60590492+DiksySingh@users.noreply.github.com>
2024-10-10 15:57:05 +0530

    ss

--- Server/middlewares/authMiddlewares.js
+++ Server/middlewares/authMiddlewares.js
@@ -8,7 +8,6 @@ module.exports.userVerification = (allowedRoles) => {
     return async (req, res, next) => {
         const token = req.cookies.accessToken || req.headers.authorization?.split(' ')[1];
         console.log(token);
-
         if (!token) {
             return res.status(400).json({ 
                 status: false, 
