cdcc7d6c2615a0409a7a63e7a68a85e5f74382fa
Dikshant Kumar Singh <60590492+DiksySingh@users.noreply.github.com>
2024-10-23 11:06:59 +0530

    Changes

--- Server/controllers/itemController.js
+++ Server/controllers/itemController.js
@@ -1,138 +1,197 @@
 const Item = require("../models/itemSchema");
-
+const IncomingItem = require("../models/incomingItemSchema");
 //Add New Item
-module.exports.addItem = async(req, res) => {
-        const {itemName, stock, createdAt, updatedAt} = req.body;
-        if(!itemName){
-            return res.status(400).json({
-                success: false,
-                message: "itemName is required"
-            });
-        }
+module.exports.addItem = async (req, res) => {
+  const { itemName, stock, createdAt, updatedAt } = req.body;
+  if (!itemName) {
+    return res.status(400).json({
+      success: false,
+      message: "itemName is required",
+    });
+  }
 
-        // if(!stock){
-        //     return res.status(400).json({
-        //         success: false,
-        //         message: "Stock is required"
-        //     });
-        // }
+  // if(!stock){
+  //     return res.status(400).json({
+  //         success: false,
+  //         message: "Stock is required"
+  //     });
+  // }
 
-        try{
-            const newItem = new Item({itemName, stock, createdAt, updatedAt});
-            const itemData = await newItem.save();
-            if(!itemData){
-                return res.status(400).json({
-                    success: false,
-                    message: "Data Insertion Failed"
-                });
-            }
-            
-            res.status(200).json({
-                success: true,
-                message: "Data Inserted Successfully",
-                data: itemData
-            });
-        }catch(error){
-            res.status(500).json({
-                success: false,
-                error: error.message
-            });
-        }
+  try {
+    const newItem = new Item({ itemName, stock, createdAt, updatedAt });
+    const itemData = await newItem.save();
+    if (!itemData) {
+      return res.status(400).json({
+        success: false,
+        message: "Data Insertion Failed",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Data Inserted Successfully",
+      data: itemData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 //View All Items
-module.exports.showItems = async(req, res) => {
-    try{
-        const allItems = await Item.find();
-        if(!allItems){
-            return res.status(404).json({
-                success: false,
-                message: "Data Not Found"
-            });
-        }
-        res.status(200).json({
-            success: true,
-            message: "Data Fetched Successfully",
-            data: allItems
-        });
-    }catch(error){
-        res.status(500).json({
-            success: false,
-            error: error.message
-        });
+module.exports.showItems = async (req, res) => {
+  try {
+    const allItems = await Item.find();
+    if (!allItems) {
+      return res.status(404).json({
+        success: false,
+        message: "Data Not Found",
+      });
     }
+    res.status(200).json({
+      success: true,
+      message: "Data Fetched Successfully",
+      data: allItems,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 //Update Item
-module.exports.updateItem = async(req, res) => {
-    const {id} = req.query;
-    const updates = req.body;
-    console.log(updates);
-    if(!id){
-        return res.status(400).json({
-            success: false,
-            message: "Item ID is required"
-        });
+// module.exports.updateItem = async (req, res) => {
+//   const { id } = req.query;
+//   const updates = req.body;
+//   console.log(updates);
+//   if (!id) {
+//     return res.status(400).json({
+//       success: false,
+//       message: "Item ID is required",
+//     });
+//   }
+
+//   try {
+//     const existingItem = await Item.findById({ _id: id });
+//     if (!existingItem) {
+//       return res.status(200).json({
+//         success: false,
+//         message: "Item Not Found",
+//       });
+//     }
+
+//     if (updates.itemName) {
+//       existingItem.itemName = updates.itemName;
+//     }
+//     if (updates.stock) {
+//       existingItem.stock += updates.stock;
+//     }
+
+//     await existingItem.save();
+//     const updatedItemData = await Item.findById({ _id: id });
+//     res.status(200).json({
+//       success: true,
+//       message: "Item updated successfully",
+//       item: updatedItemData,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       error: error.message,
+//     });
+//   }
+// };
+
+module.exports.incomingItems = async (req, res) => {
+  try {
+    const {
+      warehouse,
+      itemComingFrom,
+      itemName,
+      quantity,
+      defectiveItem,
+      arrivedDate,
+    } = req.body;
+    if (!warehouse || !itemComingFrom || !itemName || !quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
 
-    try{
-        const existingItem = await Item.findById({_id: id});
-        if(!existingItem){
-            return res.status(200).json({
-                success: false,
-                message: "Item Not Found",
-            });  
-        }
-
-        if(updates.itemName){
-            existingItem.itemName = updates.itemName;
-        }
-        if(updates.stock){
-            existingItem.stock += updates.stock; 
-        }
-         
-        await existingItem.save();
-        const updatedItemData = await Item.findById({_id: id});
-        res.status(200).json({
-            success: true,
-            message: "Item updated successfully",
-            item: updatedItemData,
-        });
-    }catch(error){
-        res.status(500).json({
-            success: false,
-            error: error.message
-        });
+    const foundItem = await Item.findOne({ itemName });
+    if (!foundItem) {
+      return res.status(404).json({
+        success: false,
+        message: `Item ${itemName} not found`,
+      });
     }
+    const nondefectItem = quantity - defectiveItem;
+    foundItem.stock += nondefectItem;
+    foundItem.updatedAt = Date.now();
+    await foundItem.save();
+
+    const incomingItems = new IncomingItem({
+      warehouse,
+      itemComingFrom,
+      itemName,
+      quantity,
+      defectiveItem,
+      arrivedDate,
+    });
+
+    await incomingItems.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Data Inserted Successfully",
+      incomingItems,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 };
 
-//Delete Item
-module.exports.deleteItem = async(req, res) => {
-    const {id} = req.query;
-    if(!id){
-        return res.status(400).json({
-            success: false,
-            message: "Item ID is required"
-        });
-    }
 
-    try{
-        const deletedItem = await Item.findByIdAndDelete(id);
-        if(!deletedItem){
-            return res.status(404).json({
-                success: false,
-                message: "Item Not Found"
-            });
-        }
-        res.status(200).json({
-            success: true,
-            message: "Item removed successfully",
-            data: deletedItem
-        });
-    }catch(error){
-        res.status(500).json({
-            success: false,
-            error: error.message
-        });
+//module.exports.incomingItemDetails = async(req,)
+
+
+
+//Delete Item
+module.exports.deleteItem = async (req, res) => {
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).json({
+      success: false,
+      message: "Item ID is required",
+    });
+  }
+
+  try {
+    const deletedItem = await Item.findByIdAndDelete(id);
+    if (!deletedItem) {
+      return res.status(404).json({
+        success: false,
+        message: "Item Not Found",
+      });
     }
+    res.status(200).json({
+      success: true,
+      message: "Item removed successfully",
+      data: deletedItem,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
--- Server/routes/itemRoute.js
+++ Server/routes/itemRoute.js
@@ -1,13 +1,23 @@
-const {addItem, showItems, updateItem, deleteItem} = require("../controllers/itemController");
-const {userVerification} = require("../middlewares/authMiddlewares");
+const {
+  addItem,
+  showItems,
+  updateItem,
+  deleteItem,
+  incomingItems,
+} = require("../controllers/itemController");
+const { userVerification } = require("../middlewares/authMiddlewares");
 const router = require("express").Router();
 
 //Admin Accessible Route
-router.get("/viewItems", userVerification(['admin', 'warehouseAdmin']), showItems);
+router.get(
+  "/viewItems",
+  userVerification(["admin", "warehouseAdmin"]),
+  showItems
+);
 
 //Inventory Accessible Route
-router.post("/newItem", userVerification(['warehouseAdmin']), addItem);
-router.patch("/updateItem", userVerification(['warehouseAdmin']), updateItem);
-router.delete("/deleteItem", userVerification(['warehouseAdmin']), deleteItem);
+router.post("/newItem", userVerification(["warehouseAdmin"]), addItem);
+router.post("/updateItem", userVerification(["warehouseAdmin"]), incomingItems);
+router.delete("/deleteItem", userVerification(["warehouseAdmin"]), deleteItem);
 
 module.exports = router;
