02242b6cd94738ec4b44f1d136c05ce0fd69e447
DiksySingh <singhdikshant24@gmail.com>
2024-11-13 17:06:06 +0530

    Changes

--- Server/middlewares/authMiddlewares.js
+++ Server/middlewares/authMiddlewares.js
@@ -11,7 +11,7 @@ module.exports.userVerification = (allowedRoles) => {
   return async (req, res, next) => {
     const token =
       req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
-    console.log(token);
+    //console.log(token);
     if (!token) {
       return res.status(400).json({
         status: false,
